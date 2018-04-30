8f603b307ea8258b585ede07e29a5a7435ef368c
matisapatta <mati.sapatta@gmail.com>
2018-04-30 20:10:58 -0300

    .

---
+++ SimulacionDados/Dado.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace SimulacionDados
+{
+    public class Dado
+    {
+        private int contador = 0;
+        private int num;
+        public Dado(int num)
+        {
+            this.num = num;
+        }
+
+        public void sumar(){
+            contador++;
+        }
+
+        public int mostrarNum(){
+            return this.num;
+        }
+
+        public int mostrarCont(){
+            return this.contador;
+        }
+    }
+}
--- SimulacionDados/Program.cs
+++ SimulacionDados/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SimulacionDados
 {
@@ -6,7 +7,48 @@ namespace SimulacionDados
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Inicializo los dados
+
+            int[] lista = new int[] {0,0,0,0,0,0};
+            int cantrand;
+            Random rand = new Random();
+            double random = 0;
+            int max = 0;
+            int num = 0;
+         
+
+         Console.Write("Ingrese cantidad de números a generar: ");
+            int.TryParse(Console.ReadLine(), out cantrand);
+
+            for (int i = 0; i < cantrand;i++){
+                random = rand.NextDouble();
+                if(random>=0&&random<=0.166666){
+                    lista[0]++;
+                } else if(random >= 0.166667 && random <= 0.333333){
+                    lista[1]++;
+                } else if(random >= 0.333334 && random <= 0.5){
+                    lista[2]++;
+                } else if(random >= 0.500001 && random <= 0.666666){
+                    lista[3]++;
+                } else if(random >= 0.666667 && random <= 0.833333){
+                    lista[4]++;
+                } else if(random >= 0.833334 && random <= 1){
+                    lista[5]++;
+                }
+            }
+
+
+            for (int i = 0; i < 6;i++){
+                if(lista[i]>max){
+                    max = lista[i];
+                    num = (i + 1);
+                }
+                Console.WriteLine("El número " + (i + 1) + " apareció " + lista[i] + " veces.");
+            }
+
+            Console.WriteLine("El número que más apareció fue el " + num + " con " + max + " apariciones.");
+           
+            Console.ReadKey();
         }
     }
 }
